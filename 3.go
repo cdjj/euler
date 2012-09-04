3e6eb99b4d103ce41602cbce8a3405c3db0a5340
Chris Jones <cdjj@server>
2012-09-03 21:57:45 -0700

    Solution for problem 3.

---
+++ 3.go
@@ -0,0 +1,34 @@
+package main
+import (
+	"fmt"
+	"math"
+)
+
+func factors(n int64) []int64 {
+	max_factor := int64(math.Sqrt(float64(n)))
+	factors := make([]int64, 1, max_factor)
+	factors[0] = 9223372036854775807 // avoid div-by-zero in first i % factors[f] check.
+
+foreach_i:
+	for i := int64(2); i <= max_factor; i++ { 
+		if (n % i == 0) { // n is a multiple of i; now check against factors
+		for f := range factors {
+				if ((i % factors[f]) == 0) { // multiple of a known factor
+					continue foreach_i
+				}
+			}
+			// factor is prime
+			factors = factors[0:len(factors) + 1]
+			factors[len(factors) - 1] = i
+			fmt.Printf("Adding %d to %v\n", i, factors)
+		}
+	}
+	return factors[1:]
+}
+
+func main() {
+	n := int64(600851475143)
+	factors := factors(n)
+	fmt.Println("The prime factors of ", n, " are ", factors, 
+		" and the largest is ", factors[len(factors) - 1])
+}
